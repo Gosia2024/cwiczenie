675d24805dab10ba1c425014c85f899cacb14eaf
Gosia <lm.sprachen@gmail.com>
2024-07-31 08:09:54 +0200

    first commit

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [CommonModule, ],
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent {
+  posts: {
+    name: string;
+    image: string;
+    likes: number;
+    isLiked: boolean;
+  }[] = [
+    {
+      name: 'Hans',
+      image: './assets/img/banana.jpg',
+      likes: 55,
+      isLiked: true,
+    },
+    {
+      name: 'Mandy',
+      image: './assets/img/orange.jpg',
+      likes: 21,
+      isLiked: false,
+    },
+    {
+      name: 'Nadine',
+      image: './assets/img/currant.jpg',
+      likes: 274,
+      isLiked: true,
+    },
+  ];
+
+
+  toggleLike(index: number) {
+    if (this.posts[index].isLiked) {
+      this.posts[index].isLiked = false;
+      this.posts[index].likes--;
+    } else {
+      this.posts[index].isLiked = true;
+      this.posts[index].likes++;
+    }
+  }
+
+  constructor() {}
+
+
+ 
+
+  
+}
